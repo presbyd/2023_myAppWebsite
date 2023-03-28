6ad116efa23fd6c7a684b6a698733f0fc6174fcc
presbyd <david.m.presby@gmail.com>
2023-03-27 21:13:35 -0400

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
+import { Analytics } from '@vercel/analytics/react';
+
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    useEffect(() => {
-        TagManager.initialize({ gtmId: 'G-0HBQ1BX8Q4' });
-    }, []);
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+
 }
